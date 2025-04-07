cf48d531c5fc9ba02eaf63ba0e96f562ca3c8cc3
darynjack <darynjackson@unomaha.edu>
2025-04-07 04:58:40 +0000

    done

--- ProcessData.py
+++ ProcessData.py
@@ -4,6 +4,25 @@
 #Assignment:
 
 import random
+def makeID(first, last, ID):
+  userID = first[0] + last +ID[8:]
+
+  return userID
+
+def makeMajorYear(major, year):
+
+  if year == "Freshman":
+    year = "Fr"
+  if year == "Sophomore":
+    year = "So"
+  if year == "Junior":
+    year = "Jr"
+  if year == "Senior":
+    year = "Sr"
+ 
+  majorYear = major[0:3] + year
+ 
+  return majorYear
 
 def main():
 
@@ -12,9 +31,18 @@ def main():
   outFile = open("StudentList.csv", 'w')
 
   #Process each line of the input file and output to the CSV file
+  output = ""
+  for student in inFile:
+    data = student.split()
+  # print(data)
+    userID = makeID(data[0], data[1], data[3])
+    print(userID)
+    majorYear =makeMajorYear(data[6], data[5])
+    print(majorYear)
 
-
-
+    output += data[1] + "," + data[0] + "\n"
+  outFile.write(output)
+ 
   #Close files in the end to save and ensure they are not damaged.
   inFile.close()
   outFile.close()
